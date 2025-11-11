c80f9c23b1349c3ef6e9c1477306f7abde99e8b2
developer-bappeda <gigihtrianung@gmail.com>
2025-11-11 09:58:50 +0700

    edit config routes dan filters

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,13 +32,16 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
+//landigpage routes
+$routes->get('/landingpage', 'Landingpage::index');
+
 //global routes
-$routes->get('/', 'Home::index');
-$routes->get('/gantipassword', 'Home::gantipassword');
-$routes->get('/home/updatepassword', 'Home::updatepassword');
+$routes->get('/', 'Home::index', ['filter' => 'role:admin,useropd']);
+$routes->get('/gantipassword', 'Home::gantipassword', ['filter' => 'role:admin,useropd']);
+$routes->get('/home/updatepassword', 'Home::updatepassword', ['filter' => 'role:admin,useropd']);
 
 //YEARS PICK
-$routes->post('/home/saveyears', 'Home::saveyears');
+$routes->post('/home/saveyears', 'Home::saveyears', ['filter' => 'role:admin,useropd']);
 
 //REAL INDEX AFTER CHOOSE YEARS
 // $routes->get('/home/realindex', 'Home::realindex');
@@ -52,57 +55,57 @@ $routes->get('/usulanhibahadmin', 'Usulanhibahadmin::index', ['filter' => 'role:
 $routes->match(['get', 'post'], 'setting/users/datatable', 'Home::datatableusers', ['filter' => 'role:admin']);
 
 // Master-Bansos
-$routes->get('master/bansos', 'MasterBansos::index');
-$routes->match(['get', 'post'], 'master/bansos/datatable', 'MasterBansos::datatable');
-$routes->get('master/bansos/create', 'MasterBansos::create');
-$routes->post('master/bansos/store', 'MasterBansos::store');
-$routes->get('master/bansos/edit/(:num)', 'MasterBansos::edit/$1');
-$routes->post('master/bansos/update', 'MasterBansos::update');
-$routes->get('master/bansos/delete/(:num)', 'MasterBansos::delete/$1');
-$routes->post('master/bansos/detail-json', 'MasterBansos::detailJson');
+$routes->get('master/bansos', 'MasterBansos::index', ['filter' => 'role:admin,useropd']);
+$routes->match(['get', 'post'], 'master/bansos/datatable', 'MasterBansos::datatable', ['filter' => 'role:admin,useropd']);
+$routes->get('master/bansos/create', 'MasterBansos::create', ['filter' => 'role:admin,useropd']);
+$routes->post('master/bansos/store', 'MasterBansos::store', ['filter' => 'role:admin,useropd']);
+$routes->get('master/bansos/edit/(:num)', 'MasterBansos::edit/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('master/bansos/update', 'MasterBansos::update', ['filter' => 'role:admin,useropd']);
+$routes->get('master/bansos/delete/(:num)', 'MasterBansos::delete/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('master/bansos/detail-json', 'MasterBansos::detailJson', ['filter' => 'role:admin,useropd']);
 
 // Master-Hibah
-$routes->get('master/hibah', 'MasterHibah::index');
-$routes->match(['get', 'post'], 'master/hibah/datatable', 'MasterHibah::datatable');
-$routes->get('master/hibah/create', 'MasterHibah::create');
-$routes->post('master/hibah/store', 'MasterHibah::store');
-$routes->get('master/hibah/edit/(:num)', 'MasterHibah::edit/$1');
-$routes->post('master/hibah/update', 'MasterHibah::update');
-$routes->get('master/hibah/delete/(:num)', 'MasterHibah::delete/$1');
-$routes->post('master/hibah/detail-json', 'MasterHibah::detailJson');
+$routes->get('master/hibah', 'MasterHibah::index', ['filter' => 'role:admin,useropd']);
+$routes->match(['get', 'post'], 'master/hibah/datatable', 'MasterHibah::datatable', ['filter' => 'role:admin,useropd']);
+$routes->get('master/hibah/create', 'MasterHibah::create', ['filter' => 'role:admin,useropd']);
+$routes->post('master/hibah/store', 'MasterHibah::store', ['filter' => 'role:admin,useropd']);
+$routes->get('master/hibah/edit/(:num)', 'MasterHibah::edit/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('master/hibah/update', 'MasterHibah::update', ['filter' => 'role:admin,useropd']);
+$routes->get('master/hibah/delete/(:num)', 'MasterHibah::delete/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('master/hibah/detail-json', 'MasterHibah::detailJson', ['filter' => 'role:admin,useropd']);
 
 // Usulan-Hibah
-$routes->get('usulan/hibah', 'UsulanHibah::index');
-$routes->match(['get', 'post'], 'usulan/hibah/datatable', 'UsulanHibah::datatable');
-$routes->get('usulan/hibah/create', 'UsulanHibah::create');
-$routes->post('usulan/hibah/store', 'UsulanHibah::store');
-$routes->get('usulan/hibah/edit/(:num)', 'UsulanHibah::edit/$1');
-$routes->post('usulan/hibah/update', 'UsulanHibah::update');
-$routes->get('usulan/hibah/delete/(:num)', 'UsulanHibah::delete/$1');
+$routes->get('usulan/hibah', 'UsulanHibah::index', ['filter' => 'role:admin,useropd']);
+$routes->match(['get', 'post'], 'usulan/hibah/datatable', 'UsulanHibah::datatable', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/hibah/create', 'UsulanHibah::create', ['filter' => 'role:admin,useropd']);
+$routes->post('usulan/hibah/store', 'UsulanHibah::store', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/hibah/edit/(:num)', 'UsulanHibah::edit/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('usulan/hibah/update', 'UsulanHibah::update', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/hibah/delete/(:num)', 'UsulanHibah::delete/$1', ['filter' => 'role:admin,useropd']);
 
 // Usulan-Bansos
-$routes->get('usulan/bansos', 'UsulanBansos::index');
-$routes->match(['get', 'post'], 'usulan/bansos/datatable', 'UsulanBansos::datatable');
-$routes->get('usulan/bansos/create', 'UsulanBansos::create');
-$routes->post('usulan/bansos/store', 'UsulanBansos::store');
-$routes->get('usulan/bansos/edit/(:num)', 'UsulanBansos::edit/$1');
-$routes->post('usulan/bansos/update', 'UsulanBansos::update');
-$routes->get('usulan/bansos/delete/(:num)', 'UsulanBansos::delete/$1');
+$routes->get('usulan/bansos', 'UsulanBansos::index', ['filter' => 'role:admin,useropd']);
+$routes->match(['get', 'post'], 'usulan/bansos/datatable', 'UsulanBansos::datatable', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/bansos/create', 'UsulanBansos::create', ['filter' => 'role:admin,useropd']);
+$routes->post('usulan/bansos/store', 'UsulanBansos::store', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/bansos/edit/(:num)', 'UsulanBansos::edit/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('usulan/bansos/update', 'UsulanBansos::update', ['filter' => 'role:admin,useropd']);
+$routes->get('usulan/bansos/delete/(:num)', 'UsulanBansos::delete/$1', ['filter' => 'role:admin,useropd']);
 
 //ajax
-$routes->get('master/kecamatan/(:num)', 'MasterBansos::getKecamatan/$1');
-$routes->get('master/desa/(:num)', 'MasterBansos::getDesa/$1');
-$routes->get('master/kegiatan/(:num)', 'MasterBansos::getKegiatan/$1');
-$routes->get('master/sub-kegiatan/(:num)', 'MasterBansos::getSubKegiatan/$1');
-$routes->post('master/cek_nik', 'MasterBansos::cekNik');
-$routes->post('master/cek_no_akta', 'MasterHibah::cekNoAkta');
+$routes->get('master/kecamatan/(:num)', 'MasterBansos::getKecamatan/$1', ['filter' => 'role:admin,useropd']);
+$routes->get('master/desa/(:num)', 'MasterBansos::getDesa/$1', ['filter' => 'role:admin,useropd']);
+$routes->get('master/kegiatan/(:num)', 'MasterBansos::getKegiatan/$1', ['filter' => 'role:admin,useropd']);
+$routes->get('master/sub-kegiatan/(:num)', 'MasterBansos::getSubKegiatan/$1', ['filter' => 'role:admin,useropd']);
+$routes->post('master/cek_nik', 'MasterBansos::cekNik', ['filter' => 'role:admin,useropd']);
+$routes->post('master/cek_no_akta', 'MasterHibah::cekNoAkta', ['filter' => 'role:admin,useropd']);
 
 // import-excel
-$routes->get('import-excel-hibah', 'ImportExcel::index_hibah');
-$routes->post('import-excel-hibah/do', 'ImportExcel::doImportHibah');
+$routes->get('import-excel-hibah', 'ImportExcel::index_hibah', ['filter' => 'role:admin,useropd']);
+$routes->post('import-excel-hibah/do', 'ImportExcel::doImportHibah', ['filter' => 'role:admin,useropd']);
 
-$routes->get('import-excel-bansos', 'ImportExcel::index_bansos');
-$routes->post('import-excel-bansos/do', 'ImportExcel::doImportBansos');
+$routes->get('import-excel-bansos', 'ImportExcel::index_bansos', ['filter' => 'role:admin,useropd']);
+$routes->post('import-excel-bansos/do', 'ImportExcel::doImportBansos', ['filter' => 'role:admin,useropd']);
 
 
 /*
